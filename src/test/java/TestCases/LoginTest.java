3e98718df84dd67c5f22bd97426e69430ae7eff9
Galentinka <galentinka1@gmail.com>
2024-12-17 23:01:38 -0500

    TestNG Framework

--- src/test/java/TestCases/LoginTest.java
+++ src/test/java/TestCases/LoginTest.java
@@ -4,6 +4,7 @@ import Pages.AccountPage;
 import Pages.LandingPage;
 import Pages.LoginPage;
 import TestBase.TestBase;
+import org.apache.poi.ss.formula.functions.T;
 import org.openqa.selenium.WebDriver;
 import org.testng.Assert;
 import org.testng.SkipException;
@@ -42,30 +43,24 @@ public class LoginTest extends TestBase {
     @Test(priority = 2)
     public void verifyLoginWithValidEmailInvalidPassword() {
 
-        loginPage = new LoginPage(driver);
         loginPage.navigateDirectlyToAccountPage(properties.getProperty("validEmail"), dataProperties.getProperty("invalidPassword"));
-
         Assert.assertTrue(loginPage.loginWaningMessage().contains(dataProperties.getProperty("invalidLoginWarning")));
     }
 
     @Test(priority = 3)
     public void verifyLoginWithInvalidEmailValidPassword() {
 
-        loginPage = new LoginPage(driver);
         loginPage.navigateDirectlyToAccountPage(dataProperties.getProperty("invalidEmail"), properties.getProperty("validPassword"));
 
         Assert.assertTrue(loginPage.loginWaningMessage().contains(dataProperties.getProperty("invalidLoginWarning")));
     }
 
     @Test(priority = 4)
-    public void loginWithInvalidCredentials() {
+    public void loginWithInvalidCredentials() throws InterruptedException {
 
 //        int i = 20 / 0;
 
-        loginPage = new LoginPage(driver);
-
         loginPage.navigateDirectlyToAccountPage(dataProperties.getProperty("invalidEmail"), dataProperties.getProperty("invalidPassword"));
-
         Assert.assertTrue(loginPage.loginWaningMessage().contains(dataProperties.getProperty("invalidLoginWarning")));
 
     }
@@ -73,12 +68,10 @@ public class LoginTest extends TestBase {
     @Test(priority = 5)
     public void loginWithNoCredentials() {
 
-
         loginPage.clickOnLoginButton();
         Assert.assertTrue(loginPage.loginWaningMessage().contains(dataProperties.getProperty("invalidLoginWarning")));
 
 //        throw new SkipException("Skipping this test due to a condition.");
-
     }
 
     @AfterMethod
